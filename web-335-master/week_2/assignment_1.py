96fd252471d8c98a9d382905071cc381d67e70b8
ky1015 <kylie.struhs@gmail.com>
2024-06-09 20:21:44 -0600

    added week 2 work

---
+++ web-335-master/week_2/assignment_1.py
@@ -0,0 +1,10 @@
+"""
+    Author: Kylie Struhs
+    Date: June 9 2024
+    File Name: assignment_1.py
+    Description: This file demonstrates use of file header comments and how to print a 
+"""
+
+# This is the line to be printed
+print("The way of water has no beginning and no end. Our hearts beat in the womb of the world. The sea is your home, before your birth and after your death. The sea gives and the sea takes. Water connects all things: life to death, darkness to light")
+print("- Tsireya, Avatar: Way of Water")
